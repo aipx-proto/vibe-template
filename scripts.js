305f80fee2637e749800f665b9739c6b0a79ae96
James Bradford <jabradford@microsoft.com>
2025-07-11 14:49:24 -0700

    update default template

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 console.log("Created with vibe template: https://github.com/aipx-proto/vibe-template");
 
+async function loadPartial(name) {
+  const res = await fetch(`components/${name}.hbs`);
+  const templateText = await res.text();
+  Handlebars.registerPartial(name, templateText);
+}
+
 async function loadTemplate(name, data, targetSelector) {
   const res = await fetch(`components/${name}.hbs`);
   const templateText = await res.text();
@@ -7,5 +13,9 @@ async function loadTemplate(name, data, targetSelector) {
   document.querySelector(targetSelector).innerHTML = template(data);
 }
 
-loadTemplate('app', {}, 'body');
-loadTemplate('example', { title: 'Vibe Template Project', description: 'Open Copilot chat to get started' }, '#app');
+async function main() {
+  await loadPartial('example');
+  await loadTemplate('app', {}, 'body');
+}
+
+main();
